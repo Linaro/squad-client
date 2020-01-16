a7b7278b3e86117217f7f70e2d1b888caf8ee8ed
Amro Hassaan <amro.hassaan@linaro.org>
2020-01-15 21:41:03 -0500

    Amro's initial commit

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 import requests
 import logging
 
-logger = logging.getLogger('models')
+logger = logging.getLogger("models")
 logger.setLevel(logging.DEBUG)
+
 
 class SquadApi:
     url = None
@@ -10,19 +11,23 @@ class SquadApi:
 
     @staticmethod
     def configure(url=None, token=None):
-        SquadApi.url = url if url[-1] is '/' else url + '/'
+        SquadApi.url = url if url[-1] is "/" else url + "/"
         SquadApi.token = token
-        logger.debug('SquadApi: url = "%s" and token = "%s"' % (SquadApi.url, 'yes' if SquadApi.token else 'no'))
+        logger.debug(
+            'SquadApi: url = "%s" and token = "%s"'
+            % (SquadApi.url, "yes" if SquadApi.token else "no")
+        )
 
     @staticmethod
     def get(endpoint, params):
-        url = '%s%s' % (SquadApi.url, endpoint)
-        logger.debug('GET %s (%s)' % (url, params))
+        url = endpoint if SquadApi.url in endpoint else SquadApi.url + endpoint #cursor or base url
+        logger.debug("GET %s (%s)" % (url, params))
         return requests.get(url=url, params=params)
 
-class SquadObject:
+
+class SquadBase:
     endpoint = None
-    attrs = None
+    attrs = []
 
     def __fill__(self, klass, results):
         objects = []
@@ -40,9 +45,9 @@ class SquadObject:
         for attr in self.attrs:
             attrs_str.append('%s: "%s"' % (attr, getattr(self, attr)))
 
-        return '%s(%s)' % (class_name, ', '.join(attrs_str))
+        return "%s(%s)" % (class_name, ", ".join(attrs_str))
 
-    def get(self, klass, filters):
+    def __get__(self, klass, filters):
         """
             Generic get method to retrieve objects from API
             how_many: number of objects to fetch, defaults to 50,
@@ -51,70 +56,108 @@ class SquadObject:
 
         objects = []
         url = klass.endpoint
-        has_next = True
         while url:
             response = SquadApi.get(url, filters)
             result = response.json()
-            objects += self.__fill__(klass, result['results'])
-            url = result['next']
+            objects += self.__fill__(klass, result["results"])
+            url = result["next"]
         return objects
 
 
-class Squad(SquadObject):
+class Squad(SquadBase):
 
     def groups(self, **filters):
-        return self.get(Group, filters)
+        return self.__get__(Group, filters)
 
     def group(self, slug, **filters):
-        filters.update({'slug': slug})
+        filters.update({"slug": slug})
         objects = self.groups(**filters)
         return objects[0]
 
 
-class Group(SquadObject):
+class Group(SquadBase):
 
-    endpoint = '/api/groups/'
-    attrs = ['id', 'url', 'slug', 'name', 'description']
+    endpoint = "api/groups/"
+    attrs = ["id", "url", "slug", "name", "description"]
 
     def projects(self, **filters):
-        filters.update({'group': self.id})
-        self.get(Project, filters)
+        filters.update({"group": self.id})
+        return self.__get__(Project, filters)
 
     def project(self, slug):
-        filters = {'slug': slug}
+        filters = {"slug": slug}
         objects = self.projects(**filters)
         return objects[0]
 
+    def __repr__(self):
+        return "%s" % self.slug
 
-class Project(SquadObject):
 
-    endpoint = '/api/projects/'
-    attrs = ['id', 'custom_email_template', 'data_retention_days', 'description',
-             'enabled_plugins_list', 'full_name', 'group', 'html_mail', 'important_metadata_keys',
-             'is_archived', 'is_public', 'moderate_notifications', 'name', 'notification_timeout',
-             'project_settings', 'slug', 'url', 'wait_before_notification']
+class Project(SquadBase):
+
+    endpoint = "api/projects/"
+    attrs = [
+        "id",
+        "custom_email_template",
+        "data_retention_days",
+        "description",
+        "enabled_plugins_list",
+        "full_name",
+        "group",
+        "html_mail",
+        "important_metadata_keys",
+        "is_archived",
+        "is_public",
+        "moderate_notifications",
+        "name",
+        "notification_timeout",
+        "project_settings",
+        "slug",
+        "url",
+        "wait_before_notification",
+    ]
 
     def builds(self, **filters):
-        filters.update({'project': self.id})
-        return self.get(Build, filters)
+        filters.update({"project": self.id})
+        return self.__get__(Build, filters)
 
     def build(self, version):
-        filters = {'version': version}
+        filters = {"version": version}
         objects = self.builds(**filters)
         return objects[0]
 
+    def __repr__(self):
+        return "%s" % self.slug
 
-class Build(SquadObject):
 
-    endpoint = '/api/builds/'
-    attrs = ['url', 'id', 'testjobs', 'status', 'metadata', 'finished',
-             'version', 'created_at', 'datetime', 'patch_id', 'keep_data', 'project',
-             'patch_source', 'patch_baseline']
+class Build(SquadBase):
+
+    endpoint = "/api/builds/"
+    attrs = [
+        "url",
+        "id",
+        "testjobs",
+        "status",
+        "metadata",
+        "finished",
+        "version",
+        "created_at",
+        "datetime",
+        "patch_id",
+        "keep_data",
+        "project",
+        "patch_source",
+        "patch_baseline",
+    ]
 
     def testruns(self, **filters):
-        filters.update({'build': self.id})
-        return self.get(TestRun, filters)
+        filters.update({"build": self.id})
+        return self.__get__(TestRun, filters)
 
-class TestRun(SquadObject):
-    
-    endpoint = ''
+    def __repr__(self):
+        return "%s" % self.version
+
+
+class TestRun(SquadBase):
+
+    endpoint = ""
