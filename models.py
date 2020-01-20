273c05743e521a994ebd998b5e43d1094f354952
Amro Hassaan <amro.hassaan@linaro.org>
2020-01-20 08:33:45 -0500

    first iteration: add tests and metrics and change returned container to dict to enable picking a repo, also add some tiny enchancements

--- models.py
+++ models.py
@@ -19,9 +19,11 @@ class SquadApi:
         )
 
     @staticmethod
-    def get(endpoint, params):
+    def get(endpoint, params, cursor=False):
         url = endpoint if SquadApi.url in endpoint else SquadApi.url + endpoint #cursor or base url
         logger.debug("GET %s (%s)" % (url, params))
+        if cursor:
+            params = None
         return requests.get(url=url, params=params)
 
 
@@ -30,12 +32,12 @@ class SquadBase:
     attrs = []
 
     def __fill__(self, klass, results):
-        objects = []
+        objects = {}
         for result in results:
             obj = klass()
             for attr in klass.attrs:
                 setattr(obj, attr, result[attr])
-            objects.append(obj)
+            objects.update({result['id']: obj})
 
         return objects
 
@@ -53,26 +55,35 @@ class SquadBase:
             how_many: number of objects to fetch, defaults to 50,
                       -1 means follow pagination
         """
-
-        objects = []
-        url = klass.endpoint
+        if klass.__name__ == "Metric":
+            tr_id = filters.get("testrun_id", None)
+            url = Metric.endpoint.format("/".join([TestRun.endpoint, str(self.id)]))
+        else:
+            url = klass.endpoint
+        objects = {}
+        limit = filters.get("limit", None)
+        count = 0
+        cursor = False
         while url:
-            response = SquadApi.get(url, filters)
+            if limit and int(limit) != -1:
+                if int(limit) == count:
+                    break
+            response = SquadApi.get(url, filters, cursor)
             result = response.json()
-            objects += self.__fill__(klass, result["results"])
+            if not result['results']:
+                break
+            count += len(result['results'])
+            objects.update(self.__fill__(klass, result["results"]))
             url = result["next"]
+            cursor = True
         return objects
 
 
 class Squad(SquadBase):
 
     def groups(self, **filters):
-        return self.__get__(Group, filters)
-
-    def group(self, slug, **filters):
-        filters.update({"slug": slug})
-        objects = self.groups(**filters)
-        return objects[0]
+        groups = self.__get__(Group, filters)
+        return list(groups.values())[0] if len(groups) == 1 else groups
 
 
 class Group(SquadBase):
@@ -82,12 +93,8 @@ class Group(SquadBase):
 
     def projects(self, **filters):
         filters.update({"group": self.id})
-        return self.__get__(Project, filters)
-
-    def project(self, slug):
-        filters = {"slug": slug}
-        objects = self.projects(**filters)
-        return objects[0]
+        projects = self.__get__(Project, filters)
+        return list(projects.values())[0] if len(projects) == 1 else projects
 
     def __repr__(self):
         return "%s" % self.slug
@@ -119,12 +126,21 @@ class Project(SquadBase):
 
     def builds(self, **filters):
         filters.update({"project": self.id})
-        return self.__get__(Build, filters)
+        builds = self.__get__(Build, filters)
+        return list(builds.values())[0] if len(builds) == 1 else builds
 
-    def build(self, version):
-        filters = {"version": version}
-        objects = self.builds(**filters)
-        return objects[0]
+    def __get__(self, klass, filters):
+        builds = super().__get__(klass, filters)
+        if builds:
+            for build in builds:
+                build = builds[build]
+                result = SquadApi.get(build.status, None)
+                status = result.json()
+                setattr(build, 'has_metrics', status['has_metrics'])
+                setattr(build, 'has_tests', status['has_tests'])
+                setattr(build, 'approved', status['approved'])
+                setattr(build, 'notified', status['notified'])
+        return builds
 
     def __repr__(self):
         return "%s" % self.slug
@@ -147,12 +163,26 @@ class Build(SquadBase):
         "keep_data",
         "project",
         "patch_source",
-        "patch_baseline",
+        "patch_baseline", #TODO add has_metrics and has_tests and milosz filters here
     ]
 
     def testruns(self, **filters):
         filters.update({"build": self.id})
-        return self.__get__(TestRun, filters)
+        testruns = self.__get__(TestRun, filters)
+        return list(testruns.values())[0] if len(testruns) == 1 else testruns
+
+    def __fill__(self, klass, results):
+        objects = {}
+        attrs = results[0].keys()
+
+        for result in results:
+            obj = klass()
+            for attr in attrs:
+                setattr(obj, attr, result[attr])
+            setattr(obj, 'has_metrics', self.has_metrics)
+            setattr(obj, 'has_tests', self.has_tests)
+            objects.update({result['id']: obj})
+        return objects
 
     def __repr__(self):
         return "%s" % self.version
@@ -160,4 +190,66 @@ class Build(SquadBase):
 
 class TestRun(SquadBase):
 
-    endpoint = ""
+    endpoint = "/api/testruns"
+
+    def get_tests(self, **filters):
+        filters.update({"test_run": self.id})
+        return self.__get__(Test, filters)
+
+    def get_metrics(self, **filters):
+        if self.has_metrics:
+            filters.update({"testrun_id": self.id})
+            return self.__get__(Metric, filters)
+        else:
+            logger.debug("Testrun %s has no metrics" % self.id)
+            print('No Metrics in this testrun')
+        return
+
+    def __fill__(self, klass, results):
+        objects = {}
+        if klass.__name__ == 'Test':
+            attrs = ['suite_id', 'suite', 'name']
+            attrs.extend(filter(lambda x: (x not in ['short_name', 'name', 'suite']), results[0].keys()))
+            for result in results:
+                obj = klass()
+                for attr in attrs:
+                    if attr == 'name':
+                        setattr(obj, attr, result['short_name'])
+                    elif attr == 'suite':
+                        setattr(obj, attr, result['name'].rsplit('/', 1)[0])
+                    elif attr == 'suite_id':
+                        setattr(obj, attr, result['suite'])
+                    else:
+                        setattr(obj, attr, result[attr])
+                objects.update({result['id']: obj})
+        elif klass.__name__ == 'Metric':
+            attrs = list()
+            attrs.extend(filter(lambda x: (x not in ['name']), results[0].keys()))
+            for result in results:
+                obj = klass()
+                for attr in attrs:
+                    setattr(obj, attr, result[attr])
+                setattr(obj, "name", result['name'].rsplit('/', 1)[1])
+                setattr(obj, "suite", result['name'].rsplit('/', 1)[0])
+                objects.update({result['metadata']: obj})
+        return objects
+
+    def __repr__(self):
+        return "%s" % self.created_at
+
+
+class Test(SquadBase):
+    endpoint = "api/tests"
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+
+class Metric(SquadBase):
+    endpoint = "{0}/metrics"
+
+    def __repr__(self):
+        return "%s:%s" % (self.metadata, self.name)
+
+
+
--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
-
+from datetime import datetime, timedelta
 from models import *
 
 SquadApi.configure(url='https://qa-reports.linaro.org')
-
+created_at = (datetime.now() - timedelta(days=2)).strftime("%Y-%m-%dT%H:%M:%S")
 squad = Squad()
-groups = squad.groups()
-for g in groups:
-    print(g)
+trs = squad.groups(slug='lkft').projects(slug='linux-next-oe-sanity').builds(limit=1, status__has_metrics=True).testruns(limit=10)
+metrics = []
+for tr in trs:
+    metric = trs[tr].get_metrics()
+    if metric:
+        metrics.append(metric)
+pass
 
-print(squad.group('lkft'))
+# groups = squad.groups()
+# for g in groups:
+#     print(g)
+#
+# print(squad.group('lkft'))
