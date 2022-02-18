b478c30eb8c87fbf3cc680d4c6ad26c69a7f37c6
Justin Cook <justin.cook@linaro.org>
2022-02-17 23:49:37 -0600

    tests: test_submit_tuxbuild: use consistent quoting
    Lets be more consistent with quoting based on the source.
    
    By default, use single quotes (string literals).
    
    If there is a string that requires variables substitution or contains
    single quotes, instead use double quotes.
    
    Signed-off-by: Justin Cook <justin.cook@linaro.org>

--- tests/test_submit_tuxbuild.py
+++ tests/test_submit_tuxbuild.py
@@ -12,7 +12,7 @@ from squad_client.utils import first
 class SubmitTuxbuildCommandTest(unittest.TestCase):
 
     testing_server = "http://localhost:%s" % settings.DEFAULT_SQUAD_PORT
-    testing_token = "193cd8bb41ab9217714515954e8724f651ef8601"
+    testing_token = '193cd8bb41ab9217714515954e8724f651ef8601'
 
     def setUp(self):
         self.squad = Squad()
@@ -20,16 +20,16 @@ class SubmitTuxbuildCommandTest(unittest.TestCase):
 
     def submit_tuxbuild(self, tuxbuild):
         argv = [
-            "./manage.py",
-            "--squad-host",
+            './manage.py',
+            '--squad-host',
             self.testing_server,
-            "--squad-token",
+            '--squad-token',
             self.testing_token,
-            "submit-tuxbuild",
-            "--group",
-            "my_group",
-            "--project",
-            "my_project",
+            'submit-tuxbuild',
+            '--group',
+            'my_group',
+            '--project',
+            'my_project',
             tuxbuild,
         ]
 
@@ -43,24 +43,24 @@ class SubmitTuxbuildCommandTest(unittest.TestCase):
             proc.ok = proc.returncode == 0
         except sp.TimeoutExpired:
             self.logger.error(
-                'Running "%s" time out after %i seconds!' % " ".join(argv)
+                "Running '%s' time out after %i seconds!" % " ".join(argv)
             )
             proc.kill()
             out, err = proc.communicate()
 
-        proc.out = out.decode("utf-8")
-        proc.err = err.decode("utf-8")
+        proc.out = out.decode('utf-8')
+        proc.err = err.decode('utf-8')
         return proc
 
-    @unittest.mock.patch.dict(os.environ, {"KERNEL_BRANCH": "master"})
+    @unittest.mock.patch.dict(os.environ, {'KERNEL_BRANCH': 'master'})
     def test_submit_tuxbuild_build(self):
-        proc = self.submit_tuxbuild("tests/data/submit/tuxbuild/build.json")
+        proc = self.submit_tuxbuild('tests/data/submit/tuxbuild/build.json')
         self.assertTrue(proc.ok, msg=proc.err)
-        self.assertTrue(proc.err.count("Submitting 1 tests, 2 metrics") == 1)
-        project = self.squad.group("my_group").project("my_project")
+        self.assertTrue(proc.err.count('Submitting 1 tests, 2 metrics') == 1)
+        project = self.squad.group('my_group').project('my_project')
 
         # Check results for next-20201021, which has 1 instances in build.json
-        build = project.build("next-20201021")
+        build = project.build('next-20201021')
 
         base_kconfig = [
             'defconfig',
@@ -73,15 +73,15 @@ class SubmitTuxbuildCommandTest(unittest.TestCase):
 
         # Make sure metadata values match expected values
         expected_metadata = {
-            'git_repo': "https://gitlab.com/Linaro/lkft/mirrors/next/linux-next",
-            'git_ref': "master",
-            'git_sha': "5302568121ba345f5c22528aefd72d775f25221e",
+            'git_repo': 'https://gitlab.com/Linaro/lkft/mirrors/next/linux-next',
+            'git_ref': 'master',
+            'git_sha': '5302568121ba345f5c22528aefd72d775f25221e',
             'git_short_log': '5302568121ba ("Add linux-next specific files for 20201021")',
-            'git_describe': "next-20201021",
-            'kconfig': base_kconfig + ["CONFIG_IGB=y", "CONFIG_UNWINDER_FRAME_POINTER=y"],
-            'kernel_version': "5.9.0",
-            'config': "https://builds.tuxbuild.com/t8NSUfTBZiSPbBVaXLH7kw/config",
-            'download_url': "https://builds.tuxbuild.com/t8NSUfTBZiSPbBVaXLH7kw/",
+            'git_describe': 'next-20201021',
+            'kconfig': base_kconfig + ['CONFIG_IGB=y', 'CONFIG_UNWINDER_FRAME_POINTER=y'],
+            'kernel_version': '5.9.0',
+            'config': 'https://builds.tuxbuild.com/t8NSUfTBZiSPbBVaXLH7kw/config',
+            'download_url': 'https://builds.tuxbuild.com/t8NSUfTBZiSPbBVaXLH7kw/',
             'duration': 541,
             'toolchain': 'gcc-9',
         }
@@ -93,47 +93,47 @@ class SubmitTuxbuildCommandTest(unittest.TestCase):
         del metadata_attrs["id"]
         self.assertEqual(sorted(expected_metadata.keys()), sorted(metadata_attrs.keys()))
 
-        environment = self.squad.group("my_group").project("my_project").environment("x86")
+        environment = self.squad.group('my_group').project('my_project').environment('x86')
         self.assertIsNotNone(environment)
 
-        suite = self.squad.group("my_group").project("my_project").suite("build")
+        suite = self.squad.group('my_group').project('my_project').suite('build')
         self.assertIsNotNone(suite)
 
-        test = first(self.squad.tests(name="gcc-9-defconfig-b9979cfa"))
-        self.assertEqual("build/gcc-9-defconfig-b9979cfa", test.name)
-        self.assertEqual("pass", test.status)
+        test = first(self.squad.tests(name='gcc-9-defconfig-b9979cfa'))
+        self.assertEqual('build/gcc-9-defconfig-b9979cfa', test.name)
+        self.assertEqual('pass', test.status)
 
-        metric = first(self.squad.metrics(name="gcc-9-defconfig-b9979cfa-warnings"))
-        self.assertEqual("build/gcc-9-defconfig-b9979cfa-warnings", metric.name)
+        metric = first(self.squad.metrics(name='gcc-9-defconfig-b9979cfa-warnings'))
+        self.assertEqual('build/gcc-9-defconfig-b9979cfa-warnings', metric.name)
         self.assertEqual(1, metric.result)
 
-        metric = first(self.squad.metrics(name="gcc-9-defconfig-b9979cfa-duration"))
-        self.assertEqual("build/gcc-9-defconfig-b9979cfa-duration", metric.name)
+        metric = first(self.squad.metrics(name='gcc-9-defconfig-b9979cfa-duration'))
+        self.assertEqual('build/gcc-9-defconfig-b9979cfa-duration', metric.name)
         self.assertEqual(541, metric.result)
 
-    @unittest.mock.patch.dict(os.environ, {"KERNEL_BRANCH": "master"})
+    @unittest.mock.patch.dict(os.environ, {'KERNEL_BRANCH': 'master'})
     def test_submit_tuxbuild_buildset(self):
-        proc = self.submit_tuxbuild("tests/data/submit/tuxbuild/buildset.json")
+        proc = self.submit_tuxbuild('tests/data/submit/tuxbuild/buildset.json')
         self.assertTrue(proc.ok, msg=proc.out)
         self.assertTrue(proc.err.count("Submitting 1 tests, 2 metrics") == 3)
 
         build = self.squad.group("my_group").project("my_project").build("next-20201030")
 
         # Make sure metadata values match expected values
-        urls = ['https://builds.tuxbuild.com/%s/' % _id for _id in ['9NeOU1kd65bhMrL4eyI2yA', 'cjLreGasHSZj3OctZlNdpw', 'x5Mi9j6xZItTGqVtOKmnVw']]
+        urls = ["https://builds.tuxbuild.com/%s/" % _id for _id in ["9NeOU1kd65bhMrL4eyI2yA", "cjLreGasHSZj3OctZlNdpw", "x5Mi9j6xZItTGqVtOKmnVw"]]
         configs = [url + "config" for url in urls]
         expected_metadata = {
-            'git_repo': "https://gitlab.com/Linaro/lkft/mirrors/next/linux-next",
-            'git_ref': "master",
-            'git_sha': "4e78c578cb987725eef1cec7d11b6437109e9a49",
-            'git_short_log': '4e78c578cb98 ("Add linux-next specific files for 20201030")',
-            'git_describe': "next-20201030",
-            'kconfig': [['allnoconfig'], ['tinyconfig'], ['x86_64_defconfig']],
-            'kernel_version': "5.10.0-rc1",
-            'config': configs,
-            'download_url': urls,
-            'duration': 541,
-            'toolchain': 'gcc-8',
+            "git_repo": "https://gitlab.com/Linaro/lkft/mirrors/next/linux-next",
+            "git_ref": "master",
+            "git_sha": "4e78c578cb987725eef1cec7d11b6437109e9a49",
+            "git_short_log": "4e78c578cb98 (\"Add linux-next specific files for 20201030\")",
+            "git_describe": "next-20201030",
+            "kconfig": [["allnoconfig"], ["tinyconfig"], ["x86_64_defconfig"]],
+            "kernel_version": "5.10.0-rc1",
+            "config": configs,
+            "download_url": urls,
+            "duration": 541,
+            "toolchain": "gcc-8",
         }
         for expected_key in expected_metadata.keys():
             self.assertEqual(expected_metadata[expected_key], getattr(build.metadata, expected_key))
@@ -151,46 +151,46 @@ class SubmitTuxbuildCommandTest(unittest.TestCase):
         suite = self.squad.group("my_group").project("my_project").suite("build")
         self.assertIsNotNone(suite)
 
-        test = first(self.squad.tests(name="gcc-8-allnoconfig"))
-        self.assertEqual("build/gcc-8-allnoconfig", test.name)
-        self.assertEqual("pass", test.status)
+        test = first(self.squad.tests(name='gcc-8-allnoconfig'))
+        self.assertEqual('build/gcc-8-allnoconfig', test.name)
+        self.assertEqual('pass', test.status)
 
-        test = first(self.squad.tests(name="gcc-8-tinyconfig"))
-        self.assertEqual("build/gcc-8-tinyconfig", test.name)
-        self.assertEqual("pass", test.status)
+        test = first(self.squad.tests(name='gcc-8-tinyconfig'))
+        self.assertEqual('build/gcc-8-tinyconfig', test.name)
+        self.assertEqual('pass', test.status)
 
-        test = first(self.squad.tests(name="gcc-8-x86_64_defconfig"))
-        self.assertEqual("build/gcc-8-x86_64_defconfig", test.name)
-        self.assertEqual("pass", test.status)
+        test = first(self.squad.tests(name='gcc-8-x86_64_defconfig'))
+        self.assertEqual('build/gcc-8-x86_64_defconfig', test.name)
+        self.assertEqual('pass', test.status)
 
-        metric = first(self.squad.metrics(name="gcc-8-allnoconfig-warnings"))
-        self.assertEqual("build/gcc-8-allnoconfig-warnings", metric.name)
+        metric = first(self.squad.metrics(name='gcc-8-allnoconfig-warnings'))
+        self.assertEqual('build/gcc-8-allnoconfig-warnings', metric.name)
         self.assertEqual(0, metric.result)
 
-        metric = first(self.squad.metrics(name="gcc-8-tinyconfig-warnings"))
-        self.assertEqual("build/gcc-8-tinyconfig-warnings", metric.name)
+        metric = first(self.squad.metrics(name='gcc-8-tinyconfig-warnings'))
+        self.assertEqual('build/gcc-8-tinyconfig-warnings', metric.name)
         self.assertEqual(0, metric.result)
 
-        metric = first(self.squad.metrics(name="gcc-8-x86_64_defconfig-warnings"))
-        self.assertEqual("build/gcc-8-x86_64_defconfig-warnings", metric.name)
+        metric = first(self.squad.metrics(name='gcc-8-x86_64_defconfig-warnings'))
+        self.assertEqual('build/gcc-8-x86_64_defconfig-warnings', metric.name)
         self.assertEqual(0, metric.result)
 
-        metric = first(self.squad.metrics(name="gcc-8-x86_64_defconfig-duration"))
-        self.assertEqual("build/gcc-8-x86_64_defconfig-duration", metric.name)
+        metric = first(self.squad.metrics(name='gcc-8-x86_64_defconfig-duration'))
+        self.assertEqual('build/gcc-8-x86_64_defconfig-duration', metric.name)
         self.assertEqual(541, metric.result)
 
     def test_submit_tuxbuild_empty(self):
-        proc = self.submit_tuxbuild("")
+        proc = self.submit_tuxbuild('')
         self.assertFalse(proc.ok, msg=proc.err)
         self.assertIn("No such file or directory: ''", proc.err)
 
     def test_submit_tuxbuild_malformed(self):
-        proc = self.submit_tuxbuild("tests/data/submit/tuxbuild/malformed.json")
+        proc = self.submit_tuxbuild('tests/data/submit/tuxbuild/malformed.json')
         self.assertFalse(proc.ok, msg=proc.err)
-        self.assertIn("Failed to load json", proc.err)
+        self.assertIn('Failed to load json', proc.err)
 
     def test_submit_tuxbuild_missing(self):
-        proc = self.submit_tuxbuild("tests/data/submit/tuxbuild/missing.json")
+        proc = self.submit_tuxbuild('tests/data/submit/tuxbuild/missing.json')
         self.assertFalse(proc.ok)
         self.assertIn(
             "No such file or directory: 'tests/data/submit/tuxbuild/missing.json'",
@@ -199,7 +199,7 @@ class SubmitTuxbuildCommandTest(unittest.TestCase):
 
     def test_submit_tuxbuild_empty_build_status(self):
         proc = self.submit_tuxbuild(
-            "tests/data/submit/tuxbuild/empty_build_status.json"
+            'tests/data/submit/tuxbuild/empty_build_status.json'
         )
         self.assertFalse(proc.ok, msg=proc.err)
         self.assertIn(
@@ -212,7 +212,7 @@ class SubmitTuxbuildCommandTest(unittest.TestCase):
 
     def test_submit_tuxbuild_malformed_build_status(self):
         proc = self.submit_tuxbuild(
-            "tests/data/submit/tuxbuild/malformed_build_status.json"
+            'tests/data/submit/tuxbuild/malformed_build_status.json'
         )
         self.assertFalse(proc.ok, msg=proc.err)
         self.assertIn(
@@ -225,7 +225,7 @@ class SubmitTuxbuildCommandTest(unittest.TestCase):
 
     def test_submit_tuxbuild_missing_build_status(self):
         proc = self.submit_tuxbuild(
-            "tests/data/submit/tuxbuild/missing_build_status.json"
+            'tests/data/submit/tuxbuild/missing_build_status.json'
         )
         self.assertFalse(proc.ok, msg=proc.err)
         self.assertIn(
@@ -234,7 +234,7 @@ class SubmitTuxbuildCommandTest(unittest.TestCase):
         )
 
     def test_submit_tuxbuild_empty_kconfig(self):
-        proc = self.submit_tuxbuild("tests/data/submit/tuxbuild/empty_kconfig.json")
+        proc = self.submit_tuxbuild('tests/data/submit/tuxbuild/empty_kconfig.json')
         self.assertFalse(proc.ok, msg=proc.err)
         self.assertIn("Failed to validate tuxbuild data: [] is too short", proc.err)
         self.assertIn(
@@ -242,7 +242,7 @@ class SubmitTuxbuildCommandTest(unittest.TestCase):
         )
 
     def test_submit_tuxbuild_malformed_kconfig(self):
-        proc = self.submit_tuxbuild("tests/data/submit/tuxbuild/malformed_kconfig.json")
+        proc = self.submit_tuxbuild('tests/data/submit/tuxbuild/malformed_kconfig.json')
         self.assertFalse(proc.ok, msg=proc.err)
         self.assertIn(
             "Failed to validate tuxbuild data: {'CONFIG_ARM64_MODULE_PLTS': 'y'} is not of type 'string'",
@@ -254,7 +254,7 @@ class SubmitTuxbuildCommandTest(unittest.TestCase):
         )
 
     def test_submit_tuxbuild_missing_kconfig(self):
-        proc = self.submit_tuxbuild("tests/data/submit/tuxbuild/missing_kconfig.json")
+        proc = self.submit_tuxbuild('tests/data/submit/tuxbuild/missing_kconfig.json')
         self.assertFalse(proc.ok, msg=proc.err)
         self.assertIn(
             "Failed to validate tuxbuild data: 'kconfig' is a required property",
